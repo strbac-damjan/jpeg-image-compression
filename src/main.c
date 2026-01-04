da8752178f39179f5c6e1c2b7638f1746ee731a3
Strbac Damjan <>
2026-01-04 17:06:49 +0100

    Update CLI arguments and memory management

--- src/main.c
+++ src/main.c
@@ -4,15 +4,15 @@
 int main(int argc, char *argv[]) {
     // Check if sufficient arguments are provided
     // argv[0] is the program name
-    // argv[1] is the input file path (passed via INPUT=... in Make)
-    // argv[2] is the output file path (automatically set in Make)
+    // argv[1] is the input file path 
+    // argv[2] is the output file path
     // if (argc != 3) {
     //     fprintf(stderr, "Usage: %s <input_file_path> <output_file_path>\n", argv[0]);
     //     return 1;
     // }
 
     const char* inputPath = argv[1];
-    //const char* outputPath = argv[2];
+    const char* outputPath = argv[2];
 
     printf("Starting processing...\n");
     printf("Input: %s\n", inputPath);
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
     BMPImage* img = loadBMPImage(inputPath);
     
     if (img) {
-       bool value = saveJPEGGrayscale("assets/output/lena.jpeg", img);
+       bool value = saveJPEGGrayscale(outputPath, img);
        if(value) 
        {
         printf("Save is sucesfull");
