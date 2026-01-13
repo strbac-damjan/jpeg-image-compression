36ddea037611b24b9bf0139c4c0322f7e249e83c
Damjan Strbac <damjan.strbac@htecgroup.com>
2026-01-13 18:06:07 +0100

    Update dct to use AAN algortihm

--- dsp_port/jpeg_compression/src/dct.c
+++ dsp_port/jpeg_compression/src/dct.c
@@ -1,135 +1,152 @@
 #ifdef __C7000__
-#include <c7x.h>
 #include "jpeg_compression.h"
 #include <math.h>
 
-/* --- DCT CONSTANTS (Global in .c file) --- */
-/* T Matrix */
-__attribute__((aligned(64))) static const float DCT_T[64] = {
-     0.353553f,  0.353553f,  0.353553f,  0.353553f,  0.353553f,  0.353553f,  0.353553f,  0.353553f,
-     0.490393f,  0.415735f,  0.277785f,  0.097545f, -0.097545f, -0.277785f, -0.415735f, -0.490393f,
-     0.461940f,  0.191342f, -0.191342f, -0.461940f, -0.461940f, -0.191342f,  0.191342f,  0.461940f,
-     0.415735f, -0.097545f, -0.490393f, -0.277785f,  0.277785f,  0.490393f,  0.097545f, -0.415735f,
-     0.353553f, -0.353553f, -0.353553f,  0.353553f,  0.353553f, -0.353553f, -0.353553f,  0.353553f,
-     0.277785f, -0.490393f,  0.097545f,  0.415735f, -0.415735f, -0.097545f,  0.490393f, -0.277785f,
-     0.191342f, -0.461940f,  0.461940f, -0.191342f, -0.191342f,  0.461940f, -0.461940f,  0.191342f,
-     0.097545f, -0.277785f,  0.415735f, -0.490393f,  0.490393f, -0.415735f,  0.277785f, -0.097545f
-};
-/* T Transpose */
-__attribute__((aligned(64))) static const float DCT_T_TRANSPOSED[64] = {
-     0.353553f,  0.490393f,  0.461940f,  0.415735f,  0.353553f,  0.277785f,  0.191342f,  0.097545f,
-     0.353553f,  0.415735f,  0.191342f, -0.097545f, -0.353553f, -0.490393f, -0.461940f, -0.277785f,
-     0.353553f,  0.277785f, -0.191342f, -0.490393f, -0.353553f,  0.097545f,  0.461940f,  0.415735f,
-     0.353553f,  0.097545f, -0.461940f, -0.277785f,  0.353553f,  0.415735f, -0.191342f, -0.490393f,
-     0.353553f, -0.097545f, -0.461940f,  0.277785f,  0.353553f, -0.415735f, -0.191342f,  0.490393f,
-     0.353553f, -0.277785f, -0.191342f,  0.490393f, -0.353553f, -0.097545f,  0.461940f, -0.415735f,
-     0.353553f, -0.415735f,  0.191342f,  0.097545f, -0.353553f,  0.490393f, -0.461940f,  0.277785f,
-     0.353553f, -0.490393f,  0.461940f, -0.415735f,  0.353553f, -0.277785f,  0.191342f, -0.097545f
-};
+/* AAN konstante */
+#define AAN_C1  0.707106781f
+#define AAN_C2  0.382683433f
+#define AAN_C3  0.541196100f
+#define AAN_C4  1.306562965f
 
 /**
- * \brief Standard Matrix Multiplication 8x8 (C = A * B)
- */static inline void matrixMul8x8(const float * __restrict A, const float * __restrict B, float * __restrict C)
+ * AAN algoritam (In-place).
+ * Ovo je "Fast Integer DCT" portovan na float.
+ * Rezultat NIJE normalizovan (vrijednosti su 8x vece od standardnog DCT).
+ */
+static inline void aanDCT8x8(float *data)
 {
-    int i, j, k;
-    
+    float tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+    float tmp10, tmp11, tmp12, tmp13;
+    float z1, z2, z3, z4, z5, z11, z13;
+    float *dataptr;
+    int ctr;
+
+    /* Pass 1: Rows */
+    dataptr = data;
     #pragma MUST_ITERATE(8, 8, 8)
-    for ( i = 0; i < 8; i++) 
-    {
-        #pragma MUST_ITERATE(8, 8, 8)
-        for ( j = 0; j < 8; j++) 
-        {
-            float sum = 0.0f; /* FIX: Deklaracija varijable PRIJE pragme za inner loop */
+    for (ctr = 0; ctr < 8; ctr++) {
+        tmp0 = dataptr[0] + dataptr[7];
+        tmp7 = dataptr[0] - dataptr[7];
+        tmp1 = dataptr[1] + dataptr[6];
+        tmp6 = dataptr[1] - dataptr[6];
+        tmp2 = dataptr[2] + dataptr[5];
+        tmp5 = dataptr[2] - dataptr[5];
+        tmp3 = dataptr[3] + dataptr[4];
+        tmp4 = dataptr[3] - dataptr[4];
 
-            #pragma MUST_ITERATE(8, 8, 8)
-            #pragma UNROLL(8)
-            for ( k = 0; k < 8; k++) 
-            {
-                /* Pristup linearnom nizu: row*8 + col */
-                sum += A[i*8 + k] * B[k*8 + j];
-            }
-            C[i*8 + j] = sum;
-        }
+        tmp10 = tmp0 + tmp3;
+        tmp13 = tmp0 - tmp3;
+        tmp11 = tmp1 + tmp2;
+        tmp12 = tmp1 - tmp2;
+
+        dataptr[0] = tmp10 + tmp11;
+        dataptr[4] = tmp10 - tmp11;
+
+        z1 = (tmp12 + tmp13) * AAN_C1;
+        dataptr[2] = tmp13 + z1;
+        dataptr[6] = tmp13 - z1;
+
+        tmp10 = tmp4 + tmp5;
+        tmp11 = tmp5 + tmp6;
+        tmp12 = tmp6 + tmp7;
+
+        z5 = (tmp10 - tmp12) * AAN_C2;
+        z2 = (tmp10 * AAN_C3) + z5;
+        z4 = (tmp12 * AAN_C4) + z5;
+        z3 = tmp11 * AAN_C1;
+
+        z11 = tmp7 + z3;
+        z13 = tmp7 - z3;
+
+        dataptr[5] = z13 + z2;
+        dataptr[3] = z13 - z2;
+        dataptr[1] = z11 + z4;
+        dataptr[7] = z11 - z4;
+
+        dataptr += 8;
+    }
+
+    /* Pass 2: Columns */
+    dataptr = data;
+    #pragma MUST_ITERATE(8, 8, 8)
+    for (ctr = 0; ctr < 8; ctr++) {
+        tmp0 = dataptr[8*0] + dataptr[8*7];
+        tmp7 = dataptr[8*0] - dataptr[8*7];
+        tmp1 = dataptr[8*1] + dataptr[8*6];
+        tmp6 = dataptr[8*1] - dataptr[8*6];
+        tmp2 = dataptr[8*2] + dataptr[8*5];
+        tmp5 = dataptr[8*2] - dataptr[8*5];
+        tmp3 = dataptr[8*3] + dataptr[8*4];
+        tmp4 = dataptr[8*3] - dataptr[8*4];
+
+        tmp10 = tmp0 + tmp3;
+        tmp13 = tmp0 - tmp3;
+        tmp11 = tmp1 + tmp2;
+        tmp12 = tmp1 - tmp2;
+
+        dataptr[8*0] = tmp10 + tmp11;
+        dataptr[8*4] = tmp10 - tmp11;
+
+        z1 = (tmp12 + tmp13) * AAN_C1;
+        dataptr[8*2] = tmp13 + z1;
+        dataptr[8*6] = tmp13 - z1;
+
+        tmp10 = tmp4 + tmp5;
+        tmp11 = tmp5 + tmp6;
+        tmp12 = tmp6 + tmp7;
+
+        z5 = (tmp10 - tmp12) * AAN_C2;
+        z2 = (tmp10 * AAN_C3) + z5;
+        z4 = (tmp12 * AAN_C4) + z5;
+        z3 = tmp11 * AAN_C1;
+
+        z11 = tmp7 + z3;
+        z13 = tmp7 - z3;
+
+        dataptr[8*5] = z13 + z2;
+        dataptr[8*3] = z13 - z2;
+        dataptr[8*1] = z11 + z4;
+        dataptr[8*7] = z11 - z4;
+
+        dataptr++;
     }
 }
 
-/**
- * \brief Computes DCT for a single 8x8 block (Float Input).
- * \param src_block Pointer to 8x8 float block (linear, 64 elements).
- * \param dct_out   Pointer to output float buffer (linear, 64 elements).
- */
-static void computeDCTBlock(const float * __restrict src_block, float * __restrict dct_out)
-{
-    /* Aligned buffers for intermediate results */
-    __attribute__((aligned(64))) float temp[64];
-    __attribute__((aligned(64))) float result[64];
-    
-    int i;
-
-    /* * VIŠE NEMA KONVERZIJE OVDJE. 
-     * Pretpostavljamo da je 'src_block' već float i linearan (0..63).
-     */
-
-    /* Step A: Temp = Block * T' */
-    /* src_block je A, DCT_T_TRANSPOSED je B */
-    matrixMul8x8(src_block, DCT_T_TRANSPOSED, temp);
-
-    /* Step B: Result = T * Temp */
-    /* DCT_T je A, temp je B */
-    matrixMul8x8(DCT_T, temp, result);
-
-    /* Store Result */
-    #pragma MUST_ITERATE(64, 64, 64)
-    for( i = 0; i < 64; i++)
-    {
-        dct_out[i] = result[i];
-    }
-}
-/**
- * \brief Computes DCT for a 32x8 Macro Block.
- * Handles int8 -> float conversion here.
- */
 void computeDCTBlock4x8x8(const int8_t * __restrict src_data, float * __restrict dct_out, int32_t stride) 
 {
-    /* * Buffer za jedan 8x8 blok u float formatu.
-     * Alociramo ga na stacku s poravnanjem za vektorske instrukcije.
-     */
-    __attribute__((aligned(64))) float work_block[64];
-    
-    int k, r, c;
+    int k, r, c, i;
 
-    /* Procesiramo 4 bloka horizontalno (0, 1, 2, 3) */
+    // Process 4 blocks
     #pragma MUST_ITERATE(4, 4, 4)
-    #pragma UNROLL(4) // Pokušaj unrollati vanjsku petlju ako compiler dozvoli
     for (k = 0; k < 4; k++)
     {
-        /* * 1. LOAD & CONVERT
-         * Izvlačimo 8x8 blok iz 32x8 int8 buffera i prebacujemo u float buffer.
-         */
-        
-        // Pointer na početak trenutnog 8x8 bloka u int8 bufferu
-        const int8_t *block_src_start = src_data + (k * 8); 
+        const int8_t *block_src = src_data + (k * 8);
+        float *block_dst        = dct_out + (k * 64);
 
-        #pragma MUST_ITERATE(8, 8, 8)
-        #pragma UNROLL(8)
-        for(r = 0; r < 8; r++)
-        {
-            // Pointer na trenutni red unutar bloka
-            const int8_t *row_ptr = block_src_start + (r * stride);
-            
-            #pragma MUST_ITERATE(8, 8, 8)
-            for(c = 0; c < 8; c++)
-            {
-                // Linearni index za work_block (0..63)
-                work_block[r*8 + c] = (float)row_ptr[c];
+        /* 1. LOAD & CONVERT (NO LEVEL SHIFT)
+         * Posto su podaci vec centralizovani (-128..127), samo ih kastujemo u float.
+         * Nema oduzimanja 128.0f.
+         */
+        for (r = 0; r < 8; r++) {
+            const int8_t *row_ptr = block_src + (r * stride);
+            for (c = 0; c < 8; c++) {
+                block_dst[r*8 + c] = (float)row_ptr[c];
             }
         }
 
-        /* * 2. COMPUTE DCT
-         * Pozivamo ažuriranu funkciju koja prima float.
-         * Output ide direktno u završni buffer.
+        /* 2. COMPUTE AAN DCT */
+        aanDCT8x8(block_dst);
+
+        /* 3. NORMALIZE (SCALING) - KLJUČNO!
+         * AAN algoritam vraca sume koje su 8x vece od standardnog DCT-a.
+         * MatrixMul metoda je imala "ugradjenu" normalizaciju (1/sqrt(8)).
+         * Ovdje moramo rucno podijeliti sa 8 da bi rezultat bio kompatibilan
+         * sa tvojim postojecim Huffman/Quantization koracima.
          */
-        computeDCTBlock(work_block, dct_out + (k * 64));
+        #pragma MUST_ITERATE(64, 64, 64)
+        for( i = 0; i < 64; i++) {
+            block_dst[i] *= 0.125f; // Mnozenje sa 1/8
+        }
     }
 }
 #endif
